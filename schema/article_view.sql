abcc219534d33424c648092b623d709eb40795f1
mjwoo1 <mjwoo@dhicc.com>
2025-06-12 10:46:17 +0900

    chore: import event-relay modules in microservices

--- schema/article_view.sql
+++ schema/article_view.sql
@@ -1,6 +1,6 @@
 create database article_view;
 
-# 게시글 조회수 백업 용 테이블
+-- # 게시글 조회수 백업 용 테이블
 create table article_view.article_view_count
 (
     article_id bigint not null primary key,
--- service/comment/src/main/java/board/comment/CommentApplication.java
+++ service/comment/src/main/java/board/comment/CommentApplication.java
@@ -2,8 +2,12 @@ package board.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "board")
+@EnableJpaRepositories(basePackages = "board")
+@SpringBootApplication()
 public class CommentApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentApplication.class, args);
