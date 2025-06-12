6f1f3dd26c38336e161f9327e7bc2d6ee7453e1d
mjwoo1 <mjwoo@dhicc.com>
2025-06-12 15:30:57 +0900

    docs: add outbox schema

--- schema/article_view.sql
+++ schema/article_view.sql
@@ -6,3 +6,14 @@ create table article_view.article_view_count
     article_id bigint not null primary key,
     view_count bigint not null
 );
+
+create table article_view.outbox
+(
+    outbox_id  bigint        not null primary key,
+    shard_key  bigint        not null,
+    event_type varchar(100)  not null,
+    payload    varchar(5000) not null,
+    created_at datetime      not null
+);
+
+create index idx_shard_key_created_at on article_view.outbox (shard_key asc, created_at asc);
--- schema/comment.sql
+++ schema/comment.sql
@@ -15,13 +15,13 @@ create index idx_article_id_parent_comment_id_comment_id on comment.comment (art
 
 create table comment.comment_v2
 (
-    comment_id bigint                                                not null primary key,
-    content    varchar(3000)                                         not null,
-    article_id bigint                                                not null,
-    writer_id  bigint                                                not null,
+    comment_id bigint        not null primary key,
+    content    varchar(3000) not null,
+    article_id bigint        not null,
+    writer_id  bigint        not null,
     path       varchar(25) character set utf8mb4 collate utf8mb4_bin not null,
-    deleted    bool                                                  not null,
-    created_at datetime                                              not null
+    deleted    bool          not null,
+    created_at datetime      not null
 );
 
 create unique index idx_article_id_path on comment.comment_v2 (article_id asc, path asc);
